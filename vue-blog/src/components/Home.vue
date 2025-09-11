38a0c4f78b63a45174e0ffb0fe3e08d49362f0ff
Arpeet Naik <arpeetnaik31@yahoo.com>
2025-09-11 00:14:16 -0400

    Home page additions

--- vue-blog/src/components/Home.vue
+++ vue-blog/src/components/Home.vue
@@ -1,9 +1,41 @@
 <template>
-  <h1 style = "text-align: center; margin-top: 10%;">Blog Coming Soon...</h1>
+  <img src="/img/Home.JPG" alt="Home" class="hero-img"></img>
+
+  <h3 class="latest-posts">Latest Posts</h3>
 </template>
 
 <script>
 </script>
 
 <style>
+.hero-img {
+  width: 100%;           
+  max-height: 80%;    
+  object-fit:cover;    
+  display: block;     
+  border-radius: 6%;
+}
+.latest-posts{
+  text-align: center;
+  align-items: center;
+  margin-top: 3%;
+  display: flex;
+  margin: 20px 0;
+  padding: 0 40px;
+}
+
+.latest-posts::before,
+.latest-posts::after {
+  content: "";
+  flex: 1;                  
+  border-bottom: 1px solid #000; 
+}
+
+.latest-posts::before {
+  margin-right: 15px;   
+}
+
+.latest-posts::after {
+  margin-left: 15px;
+}
 </style>
